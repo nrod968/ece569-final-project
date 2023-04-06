de22fa87c3c6138de849180255da0faa19c746bc
SenselessDenial <70228783+SenselessDenial@users.noreply.github.com>
2023-04-06 14:57:12 -0700

    Cleaned & organized code

--- ece569 project/ECE569Project/Project/kernel.cu
+++ ece569 project/ECE569Project/Project/kernel.cu
@@ -1,6 +1,9 @@
-#define PI 3.141592654f
+//
+// kernel.cu
 
-__global__ void colToGray(float *inImage, float *outImage, int width, int height, int numChannels) {
+///////////////////////// colToGray
+
+__global__ void colToGray_v0(float *inImage, float *outImage, int width, int height, int numChannels) {
 
     // Determine column and row of thread
     int col = threadIdx.x + blockIdx.x * blockDim.x;
@@ -17,9 +20,62 @@ __global__ void colToGray(float *inImage, float *outImage, int width, int height
   
       outImage[index] = (0.21 * r + 0.71 * g + 0.07 * b);
     }
-  }
+}
 
-__global__ void canny_edge_0(float *imageIn, float *gradient, float *angle, int width, int height) {
+__global__ void colToGray_v1_0(float *inImage, int imageArea) {
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    if (idx >= imageArea * 3) return;
+    inImage[idx] = inImage[idx] * 0.07;
+}
+
+__global__ void colToGray_v1_1(float *inImage, float *outImage, int imageArea) {
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    if (idx >= imageArea) return;
+
+    int rgbIndex = idx * 3;
+
+    float r = inImage[rgbIndex];
+    float g = inImage[rgbIndex + 1];
+    float b = inImage[rgbIndex + 2];
+  
+    outImage[idx] = (3 * r + 10 * g + b);
+}
+
+__global__ void colToGray_v2(float *inImage, float *outImage, int imageArea) {
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    if (idx >= imageArea) return;
+
+    int rgbIndex = idx * 3;
+
+    float r = inImage[rgbIndex];
+    float g = inImage[rgbIndex + 1];
+    float b = inImage[rgbIndex + 2];
+  
+    outImage[idx] = (0.21 * r + 0.71 * g + 0.07 * b);
+}
+
+  // num threads = image area * 3
+__global__ void colToGray_v3_0(float *inImage, float *intImage, int imageArea) {
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    if (idx >= imageArea * 3) return;
+    int outIdx = (idx % 3) * imageArea + (idx / 3);
+    intImage[outIdx] = inImage[idx];
+}
+
+__global__ void colToGray_v3_1(float *intImage, float *outImage, int imageArea) {
+    int idx = threadIdx.x + blockIdx.x * blockDim.x;
+    if (idx >= imageArea) return;
+
+    float r = intImage[idx];
+    float g = intImage[idx + imageArea];
+    float b = intImage[idx + imageArea + imageArea];
+  
+    outImage[idx] = (0.21 * r + 0.71 * g + 0.07 * b);
+}
+
+///////////////////////// cannyEdge
+
+__global__ void cannyEdge_v0_0(float *imageIn, float *gradient, float *angle, int width, int height) {
 
     // 0 1 2
     // 3   4
@@ -68,14 +124,168 @@ __global__ void canny_edge_0(float *imageIn, float *gradient, float *angle, int 
 
         float grad = sqrtf( pow(gx, 2) + pow(gy, 2) );
         float theta = atan2f( gy, gx );
-        theta = theta + (theta < 0) * PI;
+        theta = theta + (theta < 0) * M_PI;
 
         gradient[index] = grad;
         angle[index] = theta;
     }
 }
 
-__global__ void canny_edge_1(float *gradient, float *angle, float *edgemap, int width, int height, float lowThresh) {
+__global__ void cannyEdge_v1_0(float *imageIn, float *gradient, float *angle, int width, int height) {
+
+    // 0 1 2
+    // 3   4
+    // 5 6 7
+
+    __shared__ float n[18][18];
+
+    int col = threadIdx.x + blockIdx.x * blockDim.x; // global col
+    int row = threadIdx.y + blockIdx.y * blockDim.y; // global row
+    
+    if (col >= width || row >= height) return;
+
+    for (int i = 0; i < 18; i++) {
+        for (int j = 0; j < 18; j++) {
+            n[i][j] = 0;
+        }
+    }
+
+    __syncthreads();
+
+    int bCol = threadIdx.x; // block col
+    int bRow = threadIdx.y; // block row
+
+    int index = (row * width + col);
+
+    n[bCol + 1][bRow + 1] = imageIn[index];
+
+    if (bCol == 0 && col > 0)       n[0][bRow + 1] = imageIn[index - 1]; 
+    if (bCol == 15 && col < width - 1)  n[17][bRow + 1] = imageIn[index + 1]; 
+    if (bRow == 0 && row > 0)       n[bCol + 1][0] = imageIn[index - width];
+    if (bRow == 15 && row < height - 1) n[bCol + 1][17] = imageIn[index + width];
+
+    if (bCol == 0 && bRow == 0 && col > 0 && row > 0)            n[0][0] =  imageIn[index - width - 1];
+    if (bCol == 15 && bRow == 0 && col < width - 1 && row > 0)       n[17][0] =  imageIn[index - width + 1];
+    if (bCol == 0 && bRow == 15 && col > 0 && row < height - 1)      n[0][17] =  imageIn[index + width - 1];
+    if (bCol == 15 && bRow == 15 && col < width - 1 && row < height - 1) n[17][17] =  imageIn[index + width + 1];
+
+    __syncthreads();
+
+    float gx = -n[bCol][bRow] + n[bCol+2][bRow] - (2 * n[bCol][bRow+1]) + (2 * n[bCol+2][bRow+1]) - n[bCol][bRow+2] + n[bCol+2][bRow+2];
+    float gy = -n[bCol][bRow] + n[bCol][bRow+2] - (2 * n[bCol+1][bRow]) + (2 * n[bCol+1][bRow+2]) - n[bCol+2][bRow] + n[bCol+2][bRow+2];
+
+    float grad = sqrtf( pow(gx, 2) + pow(gy, 2) );
+    float theta = atan2f( gy, gx );
+    theta = theta + (theta < 0) * M_PI;
+
+    gradient[index] = grad;
+    angle[index] = theta;
+}
+
+__global__ void cannyEdge_v2_0(float *imageIn, float2 *out, int width, int height) {
+
+    // 0 1 2
+    // 3   4
+    // 5 6 7
+
+    float n[8];
+
+    for (int i = 0; i < 8; i++) {
+        n[i] = 0;
+    }
+
+    int col = threadIdx.x + blockIdx.x * blockDim.x;
+    int row = threadIdx.y + blockIdx.y * blockDim.y;
+
+    if (col < width && row < height) {
+        int index = (row * width + col);
+
+        if (col > 0) {
+            if (row > 0)
+                n[0] = imageIn[index - width - 1];
+
+            if (row < height - 1)
+                n[5] = imageIn[index + width - 1]; 
+
+            n[3] = imageIn[index - 1];
+        }
+
+        if (col < width - 1) {
+            if (row > 0)
+                n[2] = imageIn[index - width + 1];
+
+            if (row < height - 1)
+                n[7] = imageIn[index + width + 1];
+
+            n[4] = imageIn[index + 1];
+        }
+
+        if (row > 0) 
+            n[1] = imageIn[index - width];
+
+        if (row < height - 1)
+            n[6] = imageIn[index + width];
+
+        float gx = -n[0] + n[2] - (2 * n[3]) + (2 * n[4]) - n[5] + n[7];
+        float gy = -n[0] + n[5] - (2 * n[1]) + (2 * n[6]) - n[2] + n[7];
+
+        float grad = pow(gx, 2) + pow(gy, 2);
+        float theta = atan2f( gy, gx );
+        theta = theta + (theta < 0) * M_PI;
+
+        float2 val = make_float2(grad, theta);
+
+        out[index] = val;
+    }
+}
+
+__global__ void cannyEdge_v2_1(float2 *in, float *edgemap, int width, int height, float lowThresh) {
+
+    int col = threadIdx.x + blockIdx.x * blockDim.x;
+    int row = threadIdx.y + blockIdx.y * blockDim.y;
+
+    if (col < width && row < height) {
+        int index = (row * width + col);
+
+        float grad = in[index].x;
+        float theta = in[index].y;
+
+        float max = 1;
+
+        if ( (theta < M_PI / 8) || (theta > (7 * M_PI / 8)) ) {
+            if (col > 0 && in[index - 1].x > grad)
+                max = 0;
+            if (col < width - 1 && in[index + 1].x > grad)
+                max = 0;
+        }
+        else if ( theta < (3 * M_PI / 8) ) {
+            if (row > 0 && col > 0 && in[index - width - 1].x > grad)
+                max = 0;
+            if (row < height - 1 && col < width - 1 && in[index + width + 1].x > grad)
+                max = 0;
+        }
+        else if ( theta < (5 * M_PI / 8) ) {
+            if (row > 0 && in[index - width].x > grad)
+                max = 0;
+            if (row < height - 1 && in[index + width].x > grad)
+                max = 0;
+        }
+        else {
+            if (row > 0 && col < width - 1 && in[index - width + 1].x > grad)
+                max = 0;
+            if (row < height - 1 && col > 0 && in[index + width - 1].x > grad)
+                max = 0;
+        }
+
+        if (grad < pow(lowThresh, 2)) {
+            max = 0;
+        }
+
+        edgemap[index] = max;
+    }
+}
+
+__global__ void cannyEdge_v0_1(float *gradient, float *angle, float *edgemap, int width, int height, float lowThresh) {
 
     int col = threadIdx.x + blockIdx.x * blockDim.x;
     int row = threadIdx.y + blockIdx.y * blockDim.y;
@@ -88,19 +298,19 @@ __global__ void canny_edge_1(float *gradient, float *angle, float *edgemap, int 
 
         float max = 1;
 
-        if ( (theta < PI / 8) || (theta > (7 * PI / 8)) ) {
+        if ( (theta < M_PI / 8) || (theta > (7 * M_PI / 8)) ) {
             if (col > 0 && gradient[index - 1] > grad)
                 max = 0;
             if (col < width - 1 && gradient[index + 1] > grad)
                 max = 0;
         }
-        else if ( theta < (3 * PI / 8) ) {
+        else if ( theta < (3 * M_PI / 8) ) {
             if (row > 0 && col > 0 && gradient[index - width - 1] > grad)
                 max = 0;
             if (row < height - 1 && col < width - 1 && gradient[index + width + 1] > grad)
                 max = 0;
         }
-        else if ( theta < (5 * PI / 8) ) {
+        else if ( theta < (5 * M_PI / 8) ) {
             if (row > 0 && gradient[index - width] > grad)
                 max = 0;
             if (row < height - 1 && gradient[index + width] > grad)
@@ -121,13 +331,13 @@ __global__ void canny_edge_1(float *gradient, float *angle, float *edgemap, int 
     }
 }
 
-__global__ void apply_mask(float* inEdgemap, float* outMasked, int width, int height)
-{
+///////////////////////// applyMask
+__global__ void applyMask_v0(float* inEdgemap, float* outMasked, int width, int height) {
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
     int idy = blockIdx.y * blockDim.y + threadIdx.y;
     if (idx >= width || idy >= height) return;
 
-    // Define the points of the triangle
+    // Define the points of the triangle (bottom left corner, bottom right corner, center of image)
     float x1 = 0;
     float y1 = height - 1;
 
@@ -148,4 +358,24 @@ __global__ void apply_mask(float* inEdgemap, float* outMasked, int width, int he
     } else {
         outMasked[idy * width + idx] = 0.0;
     }
+}
+
+__global__ void applyMask_v1(float* inEdgemap, float* outMasked, int width, int height,
+                               float wX, float hY, float denom) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    int idy = blockIdx.y * blockDim.y + threadIdx.y;
+    if (idx >= width || idy >= height) return;
+
+    // Compute variables & global index
+    float xA = idx - (wX + 1);
+    float yA = idy - (hY + 1);
+    int index = idy * width + idx;
+
+    // Compute the barycentric coordinates of the current pixel
+    float alpha = (hY * xA - wX * yA) / denom;
+    float beta =  (-hY * xA - (wX+1) * yA) / denom;
+    float gamma = 1.0 - alpha - beta;
+
+    // Check if the current pixel is inside the triangle
+    outMasked[index] = (alpha >= 0.0 && beta >= 0.0 && gamma >= 0.0 && inEdgemap[index] > 0.5f);
 }
--- ece569 project/ECE569Project/Project/solution.cu
+++ ece569 project/ECE569Project/Project/solution.cu
@@ -46,6 +46,11 @@ int main(int argc, char *argv[]) {
   //float *deviceOutputImageData;
   float *gsData;
 
+  cudaEvent_t astartEvent, astopEvent;
+  float aelapsedTime;
+  cudaEventCreate(&astartEvent);
+  cudaEventCreate(&astopEvent);
+
   args = wbArg_read(argc, argv); /* parse the input arguments */
 
   inputImageFile = wbArg_getInputFile(args, 0);
@@ -72,40 +77,161 @@ int main(int argc, char *argv[]) {
              imageWidth * imageHeight * imageChannels * sizeof(float),
              cudaMemcpyHostToDevice);
 
-  ///////////////////////////////////////////////////////
- 
   // Define threads per block to be 16 x 16, and number of blocks to accomodate entire image.
-  dim3 numBlocks( ceil(imageWidth / 16.0), ceil(imageHeight / 16.0) );
-  dim3 tpb(16, 16);
+  dim3 g16x16( ceil(imageWidth / 16.0), ceil(imageHeight / 16.0) );
+  dim3 b16x16(16, 16);
+  dim3 g128( ceil(imageWidth * imageHeight / 128.0));
+  dim3 b128(128);
+  dim3 g128_tri( ceil(imageWidth * imageHeight * 3 / 128.0));
+  dim3 g64x8( ceil(imageWidth / 64.0), ceil(imageHeight / 8.0) );
+  dim3 b64x8(64, 8);
 
-  // Launch colToGray
-  colToGray<<<numBlocks, tpb>>>(deviceInputImageData, gsData, imageWidth, imageHeight, imageChannels);
+  /////////////////////////////////////////////////////// COL TO GRAY
 
-  ///////////////////////////////////////////////////////
+  // colToGray_v0
+  {
+    cudaEventRecord(astartEvent, 0);
 
-    // CANNY
+    colToGray_v0<<<g16x16, b16x16>>>(deviceInputImageData, gsData, imageWidth, imageHeight, imageChannels);
 
-    float *gradData;
-    float *angleData;
-    float *edgeData;
+    cudaEventRecord(astopEvent, 0); cudaEventSynchronize(astopEvent); cudaEventElapsedTime(&aelapsedTime, astartEvent, astopEvent);
+    printf("\nTotal compute time (ms) %f for colToGray_v0 \n", aelapsedTime);
+  }
 
-    cudaMalloc((void **)&gradData, imageWidth * imageHeight * sizeof(float));
-    cudaMalloc((void **)&angleData, imageWidth * imageHeight * sizeof(float));
-    cudaMalloc((void **)&edgeData, imageWidth * imageHeight * sizeof(float));
+  // colToGray_v1
+  {
+    cudaEventRecord(astartEvent, 0);
 
-  canny_edge_0<<<numBlocks, tpb>>>(gsData, gradData, angleData, imageWidth, imageHeight);
-  canny_edge_1<<<numBlocks, tpb>>>(gradData, angleData, edgeData, imageWidth, imageHeight, 0.2353f);
+    colToGray_v1<<<g128_tri, b128>>>(deviceInputImageData, imageWidth * imageHeight);
+    colToGray_v1<<<g128, b128>>>(deviceInputImageData, gsData, imageWidth * imageHeight);
+
+    cudaEventRecord(astopEvent, 0); cudaEventSynchronize(astopEvent); cudaEventElapsedTime(&aelapsedTime, astartEvent, astopEvent);
+    printf("\nTotal compute time (ms) %f for colToGray_v1 \n", aelapsedTime);
+  }
+  
+  // colToGray_v2
+  {
+    cudaEventRecord(astartEvent, 0);
+
+    colToGray_v2<<<g128, b128>>>(deviceInputImageData, gsData, imageWidth * imageHeight);
+
+    cudaEventRecord(astopEvent, 0); cudaEventSynchronize(astopEvent); cudaEventElapsedTime(&aelapsedTime, astartEvent, astopEvent);
+    printf("\nTotal compute time (ms) %f for colToGray_v2 \n", aelapsedTime);
+  }
+  
+  // colToGray_v3
+  {
+    float *intImageData;
+    cudaMalloc((void **)&intImageData, imageWidth * imageHeight * 3 * sizeof(float));
+    
+    cudaEventRecord(astartEvent, 0);
+
+    colToGray_v3<<<g128_tri, b128>>>(deviceInputImageData, intImageData, imageWidth * imageHeight);
+    colToGray_v3<<<g128, b128>>>(intImageData, gsData, imageWidth * imageHeight);
+
+    cudaEventRecord(astopEvent, 0); cudaEventSynchronize(astopEvent); cudaEventElapsedTime(&aelapsedTime, astartEvent, astopEvent);
+    printf("\nTotal compute time (ms) %f for colToGray_v3 \n", aelapsedTime);
+    cudaFree(intImageData);
+  }
+
+  /////////////////////////////////////////////////////// CANNY
+
+  float *gradData;
+  float *angleData;
+  float *edgeData;
+
+  cudaMalloc((void **)&gradData, imageWidth * imageHeight * sizeof(float));
+  cudaMalloc((void **)&angleData, imageWidth * imageHeight * sizeof(float));
+  cudaMalloc((void **)&edgeData, imageWidth * imageHeight * sizeof(float));
+
+  // cannyEdge_v0
+  {
+    cudaEventRecord(astartEvent, 0);
+
+    cannyEdge_v0_0<<<g16x16, b16x16>>>(gsData, gradData, angleData, imageWidth, imageHeight);
+    cannyEdge_v0_1<<<g16x16, b16x16>>>(gradData, angleData, edgeData, imageWidth, imageHeight, 0.2353f);
+
+    cudaEventRecord(astopEvent, 0); cudaEventSynchronize(astopEvent); cudaEventElapsedTime(&aelapsedTime, astartEvent, astopEvent);
+    printf("\nTotal compute time (ms) %f for cannyEdge_v0 \n", aelapsedTime);
+  }
+
+  // cannyEdge_v0 (Long)
+  {
+    cudaEventRecord(astartEvent, 0);
+
+    canny_edge_0<<<g64x8, b64x8>>>(gsData, gradData, angleData, imageWidth, imageHeight);
+    canny_edge_1<<<g64x8, b64x8>>>(gradData, angleData, edgeData, imageWidth, imageHeight, 0.2353f);
+
+    cudaEventRecord(astopEvent, 0); cudaEventSynchronize(astopEvent); cudaEventElapsedTime(&aelapsedTime, astartEvent, astopEvent);
+    printf("\nTotal compute time (ms) %f for cannyEdge_v0 (Long) \n", aelapsedTime);
+  }
+
+  // cannyEdge_v1
+  {
+    cudaEventRecord(astartEvent, 0);
+
+    canny_edge_v1_0<<<g16x16, b16x16>>>(gsData, gradData, angleData, imageWidth, imageHeight);
+    canny_edge_v0_1<<<g16x16, b16x16>>>(gradData, angleData, edgeData, imageWidth, imageHeight, 0.2353f);
+
+    cudaEventRecord(astopEvent, 0); cudaEventSynchronize(astopEvent); cudaEventElapsedTime(&aelapsedTime, astartEvent, astopEvent);
+    printf("\nTotal compute time (ms) %f for cannyEdge_v1 \n", aelapsedTime);
+  }
+  
+  // cannyEdge_v2
+  {
+    float2 *gradangleData;
+    cudaMalloc((void **)&gradangleData, imageWidth * imageHeight * sizeof(float2));
+
+    cudaEventRecord(astartEvent, 0);
+
+    cannyEdge_v2_0<<<g64x8, b64x8>>>(gsData, gradangleData, imageWidth, imageHeight);
+    cannyEdge_v2_1<<<g64x8, b64x8>>>(gradangleData, edgeData, imageWidth, imageHeight, 0.2353f);
+
+    cudaEventRecord(astopEvent, 0); cudaEventSynchronize(astopEvent); cudaEventElapsedTime(&aelapsedTime, astartEvent, astopEvent);
+    printf("\nTotal compute time (ms) %f for cannyEdge_v2 \n", aelapsedTime);
+    cudaFree(gradangleData);
+  }
 
   cudaFree(gsData);
   cudaFree(gradData);
   cudaFree(angleData);
+ 
+
+  /////////////////////////////////////////////////////// MASKING
 
   float *maskData;
-  cudaMalloc((void **)&maskData, imageWidth * imageHeight * sizeof(float));
 
-  apply_mask<<<numBlocks, tpb>>>(edgeData, maskData, imageWidth, imageHeight);
+  cudaMalloc((void **)&maskData, imageWidth * imageHeight * sizeof(float));
+  
+  // applyMask_v0
+  {
+    cudaEventRecord(astartEvent, 0);
+
+    applyMask_v0<<<g16x16, b16x16>>>(edgeData, maskData, imageWidth, imageHeight);
+
+    cudaEventRecord(astopEvent, 0); cudaEventSynchronize(astopEvent); cudaEventElapsedTime(&aelapsedTime, astartEvent, astopEvent);
+    printf("\nTotal compute time (ms) %f for applyMask_v0 \n", aelapsedTime);
+  }
+
+  // applyMask_v1
+  {
+    float wX = ((float)imageWidth / 2) - 1;                  // (width - 1) - (width / 2)
+    float hY = ((float)imageHeight / 2);                     // (height - 1) - (height / 2) + 1
+    float denom = (hY + imageWidth) - (hY * imageWidth) - 1; // Calculates barycentric denominator
+    hY -= 1;                                                 // (height - 1) - (height / 2)
+
+    cudaEventRecord(astartEvent, 0);
+
+    apply_mask_opt<<<g16x16, b16x16>>>(edgeData, maskData, imageWidth, imageHeight, wX, hY, denom);
+
+    cudaEventRecord(astopEvent, 0); cudaEventSynchronize(astopEvent); cudaEventElapsedTime(&aelapsedTime, astartEvent, astopEvent);
+    printf("\nTotal compute time (ms) %f for applyMask_v1 \n", aelapsedTime);
+  }
 
   cudaFree(edgeData);
+
+
+  /////////////////////////////////////////////////////// END
 
   cudaMemcpy(hostOutputImageData, maskData,
              imageWidth * imageHeight * sizeof(float),
