27e57531cfbd80184e9d508b6536a2da89a9e63e
Nathaniel Rodriguez <golansy0@gmail.com>
2023-04-15 16:17:10 -0700

    Bilat Filter

--- BilateralFilter.cu
+++ BilateralFilter.cu
@@ -1,42 +1,28 @@
-#define gpuErrchk(ans) { gpuAssert((ans), __FILE__, __LINE__); }
 #define M_PI 3.14159265358979323846
 
-inline void gpuAssert(cudaError_t code, char *file, int line, bool abort=true){
-	if (code != cudaSuccess){
-		fprintf(stderr, "GPUassert: %s %s %d\n", cudaGetErrorString(code), file, line);
-		if (abort) { 
-			exit(code); 
-			getchar(); 
-		}
-	}
-}
 __device__ inline float gaussian(float x, float sigma) {	
-	return 1.0f/(sigma*sqrt(2*M_PI))*exp(-(x*x) / (2 * sigma*sigma));
+	return 1.0f/(sigma*sqrtf(2*M_PI))*expf(-(x*x) / (2 * sigma*sigma));
 }
-__global__ void gpuBFCalculation(float *padimage,float *outimage,float min, float max, float *cGaussian, int pad_height, int pad_width, int kernelRadius, float sigmaD, float sigmaR) {	
+
+__global__ void gpuBFCalculation(float *input,float *output, float *cGaussian, int height, int width, int kernelWidth, float sigmaD, float sigmaR) {	
 	//Calculate our pixel's location
-	int x=blockIdx.x*blockDim.x + threadIdx.x;	
-	int y=blockIdx.y*blockDim.y + threadIdx.y;
-	int out_width=pad_width - kernelRadius * 2;
-	int out_height=pad_height - kernelRadius * 2;
+	int col=blockIdx.x*blockDim.x + threadIdx.x;	
+	int row=blockIdx.y*blockDim.y + threadIdx.y;
 	//Boundary check
 	if (x < kernelRadius || x >= (pad_height - kernelRadius) || y < kernelRadius || y >= (pad_width - kernelRadius))
 		return;
 
 	float sum = 0;
 	float totalWeight = 0;
-	float centerIntensity = padimage[x*pad_width + y];
-	float normCenterIntensity=(centerIntensity - min) / (max - min);
-	int kernelSize=kernelRadius * 2 + 1;
+	float centerIntensity = input[row * width + col];
 
-	for (int dx=x - kernelRadius; dx <= x + kernelRadius; dx++) {
-		for (int dy=y - kernelRadius; dy <= y + kernelRadius; dy++) {
-			float kernelPosIntensity=padimage[dx*pad_width + dy];			
-			float normKernelPosIntensity=(kernelPosIntensity - min) / (max - min);
-			float weight= (cGaussian[dy - y + kernelRadius] * cGaussian[dx - x + kernelRadius]) * gaussian(normKernelPosIntensity - normCenterIntensity, sigmaR);				
+	for (int dy= -1 * (kernelWidth / 2); dy <= kernelWidth / 2; dy++) {
+		for (int dx= -1 * (kernelWidth / 2); dx <= kernelWidth / 2; dx++) {
+			float kernelPosIntensity=input[(row + dy)*width + (col + dx)];			
+			float weight= (cGaussian[dy + kernelWidth / 2] * cGaussian[dx + kernelWidth / 2]) * gaussian(kernelPosIntensity - centerIntensity, sigmaR);				
 			sum+=(weight*kernelPosIntensity);
 			totalWeight+=weight;			
 		}
 	}	
-	outimage[(x - kernelRadius) * out_width + (y - kernelRadius)] = sum / totalWeight;
+	outimage[row * width + col] = sum / totalWeight;
 }
