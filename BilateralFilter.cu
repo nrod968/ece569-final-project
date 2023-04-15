e25457ff46333812bb903ef0bb50fb3e31c818b7
Nathaniel Rodriguez <golansy0@gmail.com>
2023-04-15 16:31:03 -0700

    Bilateral Filter naive

--- BilateralFilter.cu
+++ BilateralFilter.cu
@@ -4,12 +4,12 @@ __device__ inline float gaussian(float x, float sigma) {
 	return 1.0f/(sigma*sqrtf(2*M_PI))*expf(-(x*x) / (2 * sigma*sigma));
 }
 
-__global__ void gpuBFCalculation(float *input,float *output, float *cGaussian, int height, int width, int kernelWidth, float sigmaD, float sigmaR) {	
+__global__ void gpuBFCalculation(float *input,float *output, float *cGaussian, int height, int width, int kernelWidth, float sigmaR) {	
 	//Calculate our pixel's location
 	int col=blockIdx.x*blockDim.x + threadIdx.x;	
 	int row=blockIdx.y*blockDim.y + threadIdx.y;
 	//Boundary check
-	if (x < kernelRadius || x >= (pad_height - kernelRadius) || y < kernelRadius || y >= (pad_width - kernelRadius))
+	if (row >= height || col >= width)
 		return;
 
 	float sum = 0;
@@ -18,8 +18,10 @@ __global__ void gpuBFCalculation(float *input,float *output, float *cGaussian, i
 
 	for (int dy= -1 * (kernelWidth / 2); dy <= kernelWidth / 2; dy++) {
 		for (int dx= -1 * (kernelWidth / 2); dx <= kernelWidth / 2; dx++) {
+			if (row + dy < 0 || row + dy >= height || col + dx < 0 || col + dx >= width)
+				continue;
 			float kernelPosIntensity=input[(row + dy)*width + (col + dx)];			
-			float weight= (cGaussian[dy + kernelWidth / 2] * cGaussian[dx + kernelWidth / 2]) * gaussian(kernelPosIntensity - centerIntensity, sigmaR);				
+			float weight= cGaussian[(dy + kernelWidth / 2) * kernelWidth + (dx + kernelWidth / 2)] * gaussian(kernelPosIntensity - centerIntensity, sigmaR);				
 			sum+=(weight*kernelPosIntensity);
 			totalWeight+=weight;			
 		}
