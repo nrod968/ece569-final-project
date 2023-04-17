4c117815952be084d2f48187cf58db21a4bf2d17
Nathaniel Rodriguez <golansy0@gmail.com>
2023-04-17 16:13:12 -0700

    BF fixes for readability

--- BilateralFilter.cu
+++ BilateralFilter.cu
@@ -12,37 +12,40 @@ __global__ void gpuBFCalculation(float *input,float *output, float *cGaussian, i
 	//Boundary check
 	if (row >= height || col >= width)
 		return;
-
+	// keep a running sum of the weights * intensity of each pixel and of the weights
 	float sum = 0;
 	float totalWeight = 0;
-	float centerIntensity = input[row * width + col];
+	float centerIntensity = input[row * width + col]; // find the center pixel's intensity
+	int kernelRadius = (int)(kernelWidth / 2);
 
-	for (int dy= -1 * (kernelWidth / 2); dy <= kernelWidth / 2; dy++) {
-		for (int dx= -1 * (kernelWidth / 2); dx <= kernelWidth / 2; dx++) {
-			if (row + dy < 0 || row + dy >= height || col + dx < 0 || col + dx >= width)
+	// iterate through the tile to get our sum and totalWeight sum
+	for (int dy= -1 * kernelRadius; dy <= kernelRadius; ++dy) {
+		for (int dx= -1 * kernelRadius; dx <= kernelRadius; ++dx) {
+			if (row + dy < 0 || row + dy >= height || col + dx < 0 || col + dx >= width) // boundary check
 				continue;
 			float kernelPosIntensity=input[(row + dy)*width + (col + dx)];			
-			float weight= cGaussian[(dy + kernelWidth / 2) * kernelWidth + (dx + kernelWidth / 2)] * gaussian(kernelPosIntensity - centerIntensity, sigmaR);				
+			float weight= cGaussian[(dy + kernelRadius) * kernelWidth + (dx + kernelRadius)] * gaussian(centerIntensity - kernelPosIntensity, sigmaR);				
 			sum+=(weight*kernelPosIntensity);
 			totalWeight+=weight;			
 		}
 	}	
-	outimage[row * width + col] = sum / totalWeight;
+	output[row * width + col] = sum / totalWeight;
 }
 
 float* BFLaunch(float* input, int width, int height, float sigmaS, float sigmaR){
-	int kernelWidth = log2f(min(width, height));
-	float* fGaussian =  (float*)malloc((kernelRadius * 2 + 1) * sizeof(float));
+	int kernelRadius = log2f(min(width, height));
+	int kernelWidth = kernelRadius * 2 + 1;
+	float* fGaussian = (float*)malloc(kernelWidth * kernelWidth * sizeof(float));
 	float *d_cGaussian;
 	float* output,
-	for (int i = 0; i < 2 * kernelRadius + 1; ++i){
-		for (int j = 0; j < 2 * kernelRadius + 1; ++i){
+	for (int i = 0; i < kernelWidth; ++i){
+		for (int j = 0; j < kernelWidth; ++i){
 			float x = sqrtf((i - kernelRadius) * (i - kernelRadius) + (j - kernelRadius) * (j - kernelRadius));
-			fGaussian[i * (2 * kernelRadius + 1)] = gaussian(x, sigmaS);
+			fGaussian[i * kernelWidth + j] = gaussian(x, sigmaS);
 		}
 	}
-	cudaMalloc(&d_cGaussian, sizeof(float)*(kernelRadius * 2 + 1));
-	cudaMemcpy(d_cGaussian, fGaussian, sizeof(float)*(kernelRadius*2 + 1), cudaMemcpyHostToDevice);
+	cudaMalloc(&d_cGaussian, sizeof(float)*(kernelWidth * kernelWidth));
+	cudaMemcpy(d_cGaussian, fGaussian, sizeof(float)*(kernelWidth * kernelWidth), cudaMemcpyHostToDevice);
 	free(fGaussian);
 
 	float *d_input;
@@ -53,7 +56,7 @@ float* BFLaunch(float* input, int width, int height, float sigmaS, float sigmaR)
 	gpuErrchk(cudaMalloc(&d_output, sizeof(float)*width*height));
 	dim3 threadsPerBlock(16,16);//normally 16*16 is optimal
 	dim3 numBlocks(ceil((float)height / threadsPerBlock.x), ceil((float)width / threadsPerBlock.y)); 
-	gpuBFCalculation <<<numBlocks, threadsPerBlock >>> (d_input, d_output, d_cGaussian, height, width, kernelRadius, sigmaR);
+	gpuBFCalculation <<<numBlocks, threadsPerBlock >>> (d_input, d_output, d_cGaussian, height, width, kernelWidth, sigmaR);
 	gpuErrchk(cudaMemcpy(output, d_output, sizeof(float)*width*height, cudaMemcpyDeviceToHost));
 	cudaFree(d_input);
 	cudaFree(d_output);
